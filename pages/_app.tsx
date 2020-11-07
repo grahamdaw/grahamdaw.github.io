106108b00059329425a408474cb2e68741fb3010
Graham Daw <grahamdaw@users.noreply.github.com>
2020-11-07 22:17:49 +0200

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import { Grommet } from 'grommet'
+import { theme } from './theme'
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+
+function MyApp({ Component, pageProps }: AppProps)  {
+  return (
+    <Grommet theme={theme} full={true}> 
+      <Component {...pageProps} />
+    </Grommet>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,63 @@
+import { Anchor, Box, Heading, Paragraph } from 'grommet'
+import { Github, Linkedin } from 'grommet-icons'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+import styles from './index.module.css'
+
+export default function Home() {
+  return (
+    <Box 
+      className={styles.container}
+      fill={true}
+    >
+
+      <Header/>
+
+      {/* Menu/appbar can go here */}
+
+      {/* Body Wrapper */}
+      <Box 
+        tag='main'
+        direction='column'
+        align='center'
+        alignContent='center'
+        flex={"grow"}
+      >
+        <Heading level={1}>Welcome to {process.env.siteTitle}</Heading>
+        <Paragraph>
+          This a simple GitHub page that links to various other places. You can check out the source code for this page{' '}
+          <Anchor
+            href={process.env.siteSourceCode}
+            target="_blank"
+            rel="noopener noreferrer"
+            a11yTitle='Link to the source code'
+          >here</Anchor>.
+        </Paragraph>
+
+        <Box
+          direction='row'
+        >
+          <Anchor
+            className={styles.iconLink}
+            href={process.env.gitHubUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            icon={<Github />}
+            a11yTitle='Link to my GitHub'
+          />
+          <Anchor
+            className={styles.iconLink}
+            href={process.env.linkedInUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            icon={<Linkedin />}
+            a11yTitle='Link to my LinkedIn profile'
+          />
+        </Box>
+      </Box>
+
+
+      <Footer/>
+    </Box>
+  )
+}
